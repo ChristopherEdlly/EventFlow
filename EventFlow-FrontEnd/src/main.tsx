4735c5d958a2118732d9ad31b3709babd757421b
christopherEdlly <christopher.edlly@souunit.com.br>
2025-12-04 11:53:40 -0300

    Pequena otimização de requisições

--- EventFlow-FrontEnd/src/main.tsx
+++ EventFlow-FrontEnd/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
@@ -7,7 +6,5 @@ const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
